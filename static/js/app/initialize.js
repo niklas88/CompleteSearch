fee23082611d385c6591b25445663c59d8589289
Evgeny Anatskiy <evgeny.anatskiy@gmail.com>
2017-04-23 17:43:52 +0200

    fix scrollToTop

--- static/js/app/initialize.js
+++ static/js/app/initialize.js
@@ -4,6 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
     // Remove Loading animation when the page is fully loaded
     $('body').addClass('loaded');
 
+    // Scroll to top
+    // Check if the window is on the top. If not, then display the button
+    $(document).scroll(function() {
+        const level = $('.page-header').height();
+        if ($(this).scrollTop() > level) {
+            $('#scrollToTop').fadeIn();
+        } else {
+            $('#scrollToTop').fadeOut();
+        }
+    });
+
+    // Click event to scroll to top
+    $('#scrollToTop').click((e) => {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
     const app = new App();
     app.start();
 });
